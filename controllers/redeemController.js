07c931370b9226072bf00015dc9b57f480ae7a09
Akbarbar30 <mohammadakbar28@yahoo.co.id>
2019-11-26 22:31:43 +0700

    add name, add newprice, add old price, req.body

--- controllers/redeemController.js
+++ controllers/redeemController.js
@@ -14,12 +14,14 @@ function masterCouponList(req, response) {
 
     
 
-    let url = 'https://fabelio.com/ip/kursi-dacia.html';
+    let url = req.body.url;
 
     request(url, function (err, res, body) {
         if (err && res.statusCode !== 200) throw err;
         var desc = []
-        var prc;
+        var name;
+        var Nprc;
+        var Oprc;
         var img;
         let $ = cheerio.load(body);
 
@@ -30,11 +32,14 @@ function masterCouponList(req, response) {
                 desc.push(obj)
             });
         });
+        name = $('.product-info-main .page-title-wrapper .page-title span').text()
 
         img = $('#maincontent > div.columns > div > section.product-info__section.clearfix > div > div:nth-child(2) > div:nth-child(2) > img').attr("class")
         console.log("image: " , img)
-        prc = parseInt($('.product-info-main .price-box .price-wrapper').attr('data-price-amount'))
-        response.send({description : desc, price: prc , image : img})
+        Nprc = parseInt($('.product-info-main .price-box .price-wrapper').attr('data-price-amount'))
+        Oprc = parseInt($('.product-info-main .price-box .old-price .price-wrapper').attr('data-price-amount'))
+       
+        response.send({title : name , description : desc, Newprice: Nprc ,OldPrice: Oprc, image : img})
 
     })
 
